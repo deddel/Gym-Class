c4006a917f532627e6affedbc3d7c3e29b505bd6
Deddel <83078233+deddel@users.noreply.github.com>
2024-11-14 17:28:40 +0100

    Added GymClass.cs

--- Gym Class/Models/GymClass.cs
+++ Gym Class/Models/GymClass.cs
@@ -2,5 +2,12 @@
 {
     public class GymClass
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        TimeSpan Duration { get; set; }
+        public DateTime EndTime { get { return StartTime + Duration; } }
+        public string Description { get; set; }
+
     }
 }
