68a6ecbefb8082a93616b97dec2b2004f3d4cc05
Deddel <83078233+deddel@users.noreply.github.com>
2024-11-16 18:59:07 +0100

    Changed links

---
+++ Gym Class/Data/Migrations/20241116174427_Init.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gym_Class.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GymClass",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ApplicationUserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GymClass", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GymClass_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ApplicationUserGymClass",
+                columns: table => new
+                {
+                    GymClassId = table.Column<int>(type: "int", nullable: false),
+                    ApplicationUserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUserGymClass", x => new { x.ApplicationUserId, x.GymClassId });
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserGymClass_GymClass_GymClassId",
+                        column: x => x.GymClassId,
+                        principalTable: "GymClass",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUserGymClass_GymClassId",
+                table: "ApplicationUserGymClass",
+                column: "GymClassId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GymClass_ApplicationUserId",
+                table: "GymClass",
+                column: "ApplicationUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationUserGymClass");
+
+            migrationBuilder.DropTable(
+                name: "GymClass");
+        }
+    }
+}
